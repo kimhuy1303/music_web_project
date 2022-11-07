b9ebb0b7954c5e6bef98f22c93c61d47dd5c2ba5
Kim Huy <52100964@student.tdtu.edu.vn>
2022-11-07 13:10:32 +0700

    Form signin

---
+++ signin.php
@@ -0,0 +1,19 @@
+<link rel="stylesheet" href="index.css">
+<div class="wrapper">
+    <form action="" method="post">
+        <div class="logo">Archery</div>
+        <div id="form_login">
+            <div class="username">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username" placeholder="Username">
+            </div>
+            <div class="password">
+                <label for="password">Password</label>
+                <input type="text" name="password" id="password" placeholder="Password">
+                <!-- icon here -->
+            </div>
+            <input type="submit" value="Log in">
+        </div>
+        <p>Don't have account ? <a href="">Create now</a></p>
+    </form>
+</div>
