49870d57c1b4f7e6aa54ee476a7605ce7d98c0a9
Kim Huy <52100964@student.tdtu.edu.vn>
2022-11-20 13:10:38 +0700

    update cấu trúc làm

---
+++ pages/main.php
@@ -0,0 +1,15 @@
+<div class="main row col-md-10 col-sm-10">
+    <div class=" col-md-10 col-sm-10">
+        <div class="form-field">
+            <input type="text" class="form-input" placeholder="  Search for music, artists ect. ">
+            <label for="Search for music, artists ect." class="form-label"><i class='ph-magnifying-glass'
+                    style="padding-left:20px; color: gray;"></i>
+            </label>
+        </div>
+    </div>
+    <div class="col-md-2 col-sm-2">
+        <a href="login/signin.php"><input id="signin" type="button" value="Sign in">
+        </a>
+        <i class="ph-gear"></i>
+    </div>
+</div>
