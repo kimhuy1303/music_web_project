779b566805df2fbec17284b41fbb27a5a08bb60d
Quy Vy <quyvylehoang3003@gmail.com>
2022-11-21 21:22:18 +0700

    Qzy update layout slideshow

--- pages/main.php
+++ pages/main.php
@@ -1,25 +1,35 @@
-<div class="main col-md-10 col-11">
+<div class="main col-lg-10 col-11">
                 <!-- The slideshow/carousel -->
-    <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img height = "100%" width = "100%" src="./src/img/poster1.jpg" class="">
-            </div>
-            <div class="carousel-item">
-                <img height = "100%" width = "100%" src="./src/img/poster1.jpg" class="">
+    <div class="slideshow">
+        <div class="slide_wrapper">
+            <div class="slide">
+                <img class="pic1" src="./src/img/poster1.jpg">
+                <img class="pic2" src="./src/img/poster1.jpg">
+                <div class="text">
+                    <div class="title">Trending New Hits</div>
+                    <div class="name_song">Happier Than Ever</div>
+                    <div class="singer">Billie Eilish<i class="ph-play-circle"></i></div>
+                </div>
 
             </div>
-            <div class="carousel-item">
-                <img height = "100%" width = "100%" src="./src/img/poster1.jpg" class="">
-
+            <div class="slide">
+                <img class="pic1" src="./src/img/poster2.jpg">
+                <img class="pic2" src="./src/img/poster2.jpg">
+                <div class="text">
+                    <div class="title">Trending New Hits</div>
+                    <div class="name_song">Em Là</div>
+                    <div class="singer">MONO<i class="ph-play-circle"></i></div>
+                </div>
+            </div>
+            <div class="slide">
+                <img class="pic1" src="./src/img/poster3b.jpg">
+                <img class="pic2" src="./src/img/poster3b.jpg">
+                <div class="text">
+                    <div class="title">Trending New Hits</div>
+                    <div class="name_song">Sour</div>
+                    <div class="singer">Olivia Rodrigo<i class="ph-play-circle"></i></div>
+                </div>
             </div>
         </div>
-    
-    <!-- Left and right controls/icons -->
-        <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon"></span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
-            <span class="carousel-control-next-icon"></span>
-        </button>
     </div>
 </div>
--- pages/sidebar.php
+++ pages/sidebar.php
@@ -11,7 +11,7 @@
                 <span class="color_text">MENU</span>
                 <li><i class="ph-fire-simple"></i>Explore</li>
                 <li><i class="ph-users"></i>Artists</li>
-                <li><i class="ph-headphones"></i>Podcasts</li>
+                <li><i class="ph-headphones"></i>Top 100</li>
             </div>
             <!-- MY MUSIC -->
             <div class="my_music">
@@ -23,11 +23,12 @@
             <div class="playlists">
                 <span class="color_text">PLAYLISTS</span>
                 <li><i class="ph-folder-plus"></i>Create Now
-                    <div  class="name_playlists">
-                        <li>Qzy</li>
-                        <li>Chill</li>
-                    </div>
-                </li>
+            </li>
+            <!-- Khi nào tạo playlist thì show -->
+            <ul class="name_playlists">
+                <li>Qzy</li>
+                <li>Chill</li>
+            </ul>
             </div>
         </ul>
     </nav>
